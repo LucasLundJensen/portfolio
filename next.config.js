f45cfa137540dfb50f4ccb16636de800384ed0e0
Lucas Lund Jensen <lucas.lund@live.dk>
2022-12-21 00:45:25 +0100

    Typo in config

--- next.config.js
+++ next.config.js
@@ -35,7 +35,7 @@ const nextConfig = {
 			{
 				// Apply these headers to all routes in your application.
 				source: "/:path*",
-				headers: securityHeaders,
+				headers: headers,
 			},
 		];
 	},
