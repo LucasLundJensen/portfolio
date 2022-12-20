64eee01221c650c27d8dcdc15eac99f95efcae77
Lucas Lund Jensen <lucas.lund@live.dk>
2022-12-21 00:38:24 +0100

    #1 CSP Headers for NextJS Vitals

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 
 const { i18n } = require("./next-i18next.config");
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'vitals.vercel-insights.com';
+  child-src vitals.vercel-insights.com;
+  style-src 'self';
+  font-src 'self';  
+`;
+
+const headers = [
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+	},
+];
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
@@ -15,6 +30,15 @@ const nextConfig = {
 		],
 	},
 	i18n,
+	async headers() {
+		return [
+			{
+				// Apply these headers to all routes in your application.
+				source: "/:path*",
+				headers: securityHeaders,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
